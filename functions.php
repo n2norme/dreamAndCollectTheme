eb4c3bf4f42125b4870dc71bf2f5bfc44d12be45
Nicolas <nicolas.denorme@icloud.com>
2020-07-21 23:08:31 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,31 @@
+<?php
+
+// Create the Menu : 
+
+function dreamCollect_menu(){
+    register_nav_menus( array(
+        'main-menu' => 'Main Menu'
+    ));
+}
+
+//Hook 
+
+add_action('init', 'dreamCollect_menu');
+
+// Add CSS and JS : 
+
+function dreamCollect_scripts(){
+    // Bootstrap : 
+
+    wp_enqueue_style('bootstrap',get_template_directory_uri() . '/css/bootstrap.css', array(), '4.5.0');
+
+    // Google Fonts : 
+
+    wp_enqueue_style( 'googlefonts','https://fonts.googleapis.com/css2?family=Roboto&display=swap', array(), '1.0.0');
+
+    // Main CSS : 
+
+    wp_enqueue_style( 'style', get_stylesheet_directory_uri(  ), array('bootstrap','googlefonts'), '1.0.0');
+}
+
+add_action('wp_enqueue_scripts', 'dreamCollect_scripts');
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Dream&Collect - Cagnotte 100% solidaire</title>
+    <?php wp_head(); ?>
+</head>
+
+<body>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex align-items-center justify-content-around">
+            <div class="navbar__signin navbar__left justify-content-start">
+                <a class=" navbar-brand" href="./sign-in.html">
+                    <img src="./img/contact-1293388_640.png" class="navbar__signin__logo d-inline-block align-top" alt="logo" loading="lazy">
+                </a>
+            </div>
+            <div class="navbar__center justify-content-center">
+                <a class="navbar-brand m-0" href="./index.html">
+                    <img src="./img/logo-illustrator copie-1.png" class="navbar__logo" alt="logo" loading="lazy">
+                </a>
+            </div>
+            <div class="navbar__right d-flex justify-content-end">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="./list-projets.html">Voir les projets<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./depot-projet.html">Déposer votre projet</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./sign-in.html">S'identifier</a>
+                    </li>
+                </ul>
+                <?php 
+                $args = array(
+                'theme_location' => 'Main Menu',
+                'container' => 'nav',
+                'container_class' => 'main-menu'
+            );
+        
+                wp_nav_menu( $args );
+        
+                ?>
+            </div>
+    </nav>
+</header>
